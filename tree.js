3310693a5a992b68938215c3452eb83f14e925b3
Hasan Mostafa <hasanalimostafa1@gmail.com>
2025-10-27 21:03:34 +0300

    Add find(value) method to Tree class

--- tree.js
+++ tree.js
@@ -26,6 +26,89 @@ class Tree {
 
     return root;
   }
+
+  insert(value) {
+    const node = new Node(value);
+    let currentNode = this.#root;
+    while (true) {
+      if (node.data < currentNode.data) {
+        if (currentNode.left === null) {
+          currentNode.left = node;
+          break;
+        } else {
+          currentNode = currentNode.left;
+        }
+      } else if (node.data > currentNode.data) {
+        if (currentNode.right === null) {
+          currentNode.right = node;
+          break;
+        } else {
+          currentNode = currentNode.right;
+        }
+      } else {
+        break;
+      }
+    }
+  }
+
+  deleteItem(value) {
+    let currentNode = this.#root;
+    let targetNode = null;
+    while (true) {
+      if (this.#root.data === value) {
+        targetNode = this.#root;
+      } else if (currentNode.left?.data === value) {
+        targetNode = currentNode.left;
+      } else if (currentNode.right?.data === value) {
+        targetNode = currentNode.right;
+      } else {
+        currentNode =
+          value > currentNode.data ? currentNode.right : currentNode.left;
+      }
+
+      if (targetNode) {
+        let replacementNode = null;
+        // The target node has two children
+        if (targetNode.left && targetNode.right) {
+          let inorderSuccessor = targetNode.right;
+          while (inorderSuccessor.left) {
+            inorderSuccessor = inorderSuccessor.left;
+          }
+          replacementNode = inorderSuccessor;
+          this.deleteItem(inorderSuccessor.data);
+          replacementNode.left = targetNode.left;
+          replacementNode.right = targetNode.right;
+        } else {
+          // The target node is a leaf or has only one child
+          replacementNode = targetNode.left ?? targetNode.right;
+        }
+        if (targetNode === this.#root) {
+          this.#root = replacementNode;
+        } else {
+          if (targetNode === currentNode.left) {
+            currentNode.left = replacementNode;
+          } else {
+            currentNode.right = replacementNode;
+          }
+        }
+        break;
+      }
+    }
+  }
+
+  find(value) {
+    let currentNode = this.#root;
+    while (currentNode) {
+      if (value > currentNode.data) {
+        currentNode = currentNode.right;
+      } else if (value < currentNode.data) {
+        currentNode = currentNode.left;
+      } else {
+        return currentNode;
+      }
+    }
+    return null;
+  }
 }
 
 export { Tree };
